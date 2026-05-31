12b6cc8b3dd195716aae74d9255c13664c5a953d
Denys Shevchenko <denshevchenko19@gmail.com>
2026-05-31 19:25:53 +0200

    feat(frontend): init shadcn/ui

--- application/frontend/astro.config.mjs
+++ application/frontend/astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react({
-            include: ['**/react/*', '**/components/**/*.tsx', '**/components/**/*.jsx'],
-        }),
-    ],
+	integrations: [
+		react({
+			include: ["**/react/*", "**/components/**/*.tsx", "**/components/**/*.jsx"],
+		}),
+	],
 });
