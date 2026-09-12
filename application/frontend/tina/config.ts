31a7ff1fe65dc88c0d4008262cf103201e45c74e
Denys Shevchenko <denshevchenko19@gmail.com>
2026-09-12 15:35:37 +0200

    refactor: move static assets to public folder and use image-size for dimensions

--- application/frontend/tina/config.ts
+++ application/frontend/tina/config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
 	media: {
 		tina: {
 			mediaRoot: "assets",
-			publicFolder: "src",
+			publicFolder: "public",
 		},
 	},
 	// See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
