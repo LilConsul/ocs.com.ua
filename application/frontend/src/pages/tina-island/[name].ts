c33cbeb212a1e0b3e1c0467411094902c1b59b23
Denys Shevchenko <denshevchenko19@gmail.com>
2026-09-12 16:41:21 +0200

    chore: remove TinaCMS demo files and test catalogue page

--- application/frontend/src/pages/tina-island/[name].ts
+++
@@ -1,6 +0,0 @@
-import { experimental_createIslandRoute } from "@tinacms/astro/experimental";
-import type { APIRoute } from "astro";
-import { islands } from "../../lib/tina/islands";
-
-export const prerender = false;
-export const ALL: APIRoute = experimental_createIslandRoute(islands);
